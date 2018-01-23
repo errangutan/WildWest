0e49e2808ca7bdbcafce343cc6af1a5d7bf9ff84
Gitler <erranggg@gmail.com>
2018-01-24 00:44:56 +0200

    Fixed weapon movement timing

--- Test Project/Assets/scripts/WeaponController.cs
+++ Test Project/Assets/scripts/WeaponController.cs
@@ -23,33 +23,28 @@ public class WeaponController : MonoBehaviour
         cameraTf = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Transform>();
 		pc = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>();
 		rb = GetComponent<Rigidbody>();
-    }
-
-    // Use this for initialization
-    void Start()
-    {
-		tf.rotation = Quaternion.LookRotation(new Vector3(-30,0,180));
-		tf.position = cameraTf.position + tf.rotation * Quaternion.LookRotation(new Vector3(0, -90, 0)) * aimPosOffset;
-        weaponEnabled = weaponEnabledDefault;
-    }
+		weaponEnabled = weaponEnabledDefault;
+	}
 
     // Update is called once per frame
     void Update()
     {
-        if (cameraTf.rotation.eulerAngles.x > minPickupAngle && cameraTf.rotation.eulerAngles.x <= 90 && true)
+        if (cameraTf.rotation.eulerAngles.x > minPickupAngle && cameraTf.rotation.eulerAngles.x <= 90 && !weaponEnabled)
         {//And countdown is over
-            PlayerController.onLookDirValid += updateWeaponTf;
 			tf.parent = null;
             weaponEnabled = true;
+			StartCoroutine("updateWeaponTf");
         }
     }
 
-    void updateWeaponTf()
+    IEnumerator updateWeaponTf()
     {
-		tf.rotation = Quaternion.Lerp(tf.rotation, cameraTf.rotation * Quaternion.LookRotation(new Vector3(0, 90, 0)), Time.fixedDeltaTime * weaponMovementSpeed);
-        tf.position = cameraTf.position + tf.rotation * Quaternion.LookRotation(new Vector3(0, -90, 0)) * aimPosOffset;
-        rb.MovePosition(tf.position + pc.movement);
-		//tf.position = cameraTf.position;
+		while(true)
+		{
+			tf.rotation = Quaternion.Lerp(tf.rotation, cameraTf.rotation * Quaternion.LookRotation(new Vector3(0, 90, 0)), Time.fixedDeltaTime * weaponMovementSpeed);
+        	tf.position = cameraTf.position + tf.rotation * Quaternion.LookRotation(new Vector3(0, -90, 0)) * aimPosOffset;
+			yield return new WaitForFixedUpdate();
+		}
     }
 
 }
